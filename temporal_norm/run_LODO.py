4e6270b370dcbf387dc04248e64183213414a563
Antoine Collas <contact@antoinecollas.fr>
2025-03-28 12:19:10 +0100

    n epochs

--- temporal_norm/run_LODO.py
+++ temporal_norm/run_LODO.py
@@ -91,7 +91,7 @@ elif norm == "PSDNorm":
 
 print(f"Filter size: {filter_size}, Depth Norm: {depth_norm}, Norm: {norm}")
 # training
-n_epochs = 1
+n_epochs = 3
 patience = 5
 
 # idx center of the window
