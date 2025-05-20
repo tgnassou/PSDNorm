232e3d18aff239589fde8a531e1e4d996a695725
tgnassou <theo.gnassounou@gmail.com>
2025-05-20 17:04:30 +0200

    cleaned metadata files

--- temporal_norm/concatenate_metadata.py
+++ temporal_norm/concatenate_metadata.py
@@ -1,35 +1,21 @@
 # %%
 import pandas as pd
 from pathlib import Path
-from joblib import Parallel, delayed
-import argparse
-# %%
-parser = argparse.ArgumentParser()
-parser.add_argument("--dataset", type=str, required=True)
-args = parser.parse_args()
-dataset = args.dataset
-
-
-def read_csv(file):
-    return pd.read_csv(file)
-
-
-path = Path(f"metadata/{dataset}")
-metadata = pd.DataFrame()
-
-
-def process_file(file):
-    try:
-        return read_csv(file)
-    except:
-        print(f"Error reading {file}")
-
-
-files = [file for file in path.iterdir() if file.is_file()]
-
-results = Parallel(n_jobs=-1)(delayed(process_file)(file) for file in files)
-
-metadata = pd.concat(results)
-metadata.to_parquet(f"metadata/metadata_sleep_{dataset}.parquet", index=False)
 
 # %%
+
+path = Path(f"metadata/metadata_per_dataset")
+
+results = []
+for file in path.iterdir():
+    metadata = pd.read_parquet(file)
+    results.append(metadata)
+# %%
+metadata = pd.concat(results).drop(columns=["Unnamed: 0"])
+metadata["session"] = metadata["session"].astype(str)
+metadata["sub+session"] = metadata.apply(
+    lambda x: f"{x['subject_id']}_{x['session']}", axis=1
+)
+
+# %%
+metadata.to_parquet("metadata/metadata_sleep.parquet", index=False)
